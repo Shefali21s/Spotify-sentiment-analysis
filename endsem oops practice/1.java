a64ba47fde25bb1e91414686a8281c35326e7be5
Shefali <shefali21s.u@gmail.com>
2025-07-30 13:39:16 +0530

    Delete endsem oops practice directory

--- endsem oops practice/1.java
+++
@@ -1,9 +0,0 @@
-class defaultvalues{
-    public static void main(String[] args) {
-        int i = 0;
-        float j = 0.0f;
-        System.out.println("i");
-        System.out.println("j");
-    }
-
-}
